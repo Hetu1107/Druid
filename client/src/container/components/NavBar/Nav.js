8f48c6bddac7a975d6525e79beb41ff090e314ca
Hetu Patel <hetu200211@gmail.com>
2022-02-15 11:11:18 +0530

    Nav Added

--- client/src/container/components/NavBar/Nav.js
+++ client/src/container/components/NavBar/Nav.js
@@ -1,11 +1,52 @@
-import React from 'react';
-import '../../style/Nav.scss';
+import React, { useEffect } from "react";
+import "../../style/Nav.scss";
+import src from '../../images/Healer.png';
+
+const navItems = [
+  {
+    logo : 'fas fa-sign-in-alt',
+    name : 'Login'
+  },
+  {
+    logo : 'fas fa-home',
+    name : 'Home'
+  },
+  {
+    logo : 'fas fa-tasks',
+    name : 'Tasks'
+  },
+  {
+    logo : 'fas fa-calendar-check',
+    name : 'Appointment'
+  },
+  {
+    logo : 'fas fa-sign-out-alt',
+    name : 'Sign-Out'
+  }
+
+]
 function Nav() {
   return (
-    <div className='main-nav-bar'>
-      
+    <div className="main-nav-bar" id="main-nav-bar">
+      <div className="nav-toggle" id="nav-toggle">
+        <img src={src}/>
+      </div>
+      <div className="middle-nav">
+        <div className="nav-element">
+          {
+            navItems.map((res)=>{
+              return(
+                <div className="ele-logo">
+                  <i className={res.logo}></i>
+                  <span className="tooltiptext">{res.name}</span>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Nav;
